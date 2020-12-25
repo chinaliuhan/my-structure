1cb9790cd0f4f8ad8cc7749907aa2d94e7139181
liuhao <liuhao@bitz.email>
2020-12-25 16:51:30 +0800

    PHP跳表练习,练习多种跳表实现的方式

--- php/SkipList/Practice/SkipListOnRand2.php
+++ php/SkipList/Practice/SkipListOnRand2.php
@@ -6,64 +6,51 @@
  * @date   2020/12/22
  */
 
+namespace SkipList\Practice;
 
 /**
  *节点类
- * @class  Node
- * @author liuhao
- * @date   2020/12/22
+ * @class   Node2
+ * @author  liuhao
+ * @date    2020/12/22
+ * @package SkipList\Practice
  */
-class Node
+class Node2
 {
-
     /**
-     * @var string
-     */
-    public $id;
-    /**
-     * @var string|int|object
-     */
-    public $value;
-    /**
+     * 数据
      * @var int
      */
-    public $level;
+    public $data;
     /**
-     * @var array
+     * 下级节点
+     * @var Node2
      */
-    public $forward;
+    public $next = [];
+
+    /**
+     * 索引高度
+     * @var int
+     */
+    public $indexMaxLevel;
+
 
     /**
      *
-     * Node constructor.
-     * @param $value
-     * @param $level
+     * Node2 constructor.
+     * @param  int    $data
+     * @param  array  $next
+     * @param  int    $indexMaxLevel
      * @author liuhao
      * @date   2020/12/22
      */
-    public function __construct($value, $level)
+    public function __construct(int $data, array $next, int $indexMaxLevel)
     {
-        //为节点的id value level 分别复制
-        $this->id    = uniqid();
-        $this->value = $value;
-        $this->level = $level;
-
-        //初始化节点
-        for ($i = 0; $i < $level; $i++) {
-            $this->forward[$i] = 0;
-        }
+        $this->data          = $data;
+        $this->next          = $next;
+        $this->indexMaxLevel = $indexMaxLevel;
     }
 
-    /**
-     *获取当前ID
-     * @return mixed
-     * @author liuhao
-     * @date   2020/12/22
-     */
-    public function getID()
-    {
-        return $this->id;
-    }
 }
 
 /**
@@ -75,113 +62,242 @@ class Node
 class SkipListOnRand2
 {
     /**
+     * 跳表入口
+     * @var Node2
+     */
+    public $head;
+
+    /**
+     * 前跳表已经随机的最大层级
      * @var int
      */
-    public $maxLevel;
+    public $levelCount;
+
     /**
-     * @var array
+     * 索引极限高度: 2^16=65536, 2^32=42亿+
+     * @var int
      */
-    public $nodePool;
-    /**
-     * @var string
-     */
-    public $header;
+    public $indexMaxLevel = 16;
 
     /**
      *
-     * SkipList constructor.
-     * @param $indexMaxLevel
+     * SkipListOnRand2 constructor.
      * @author liuhao
      * @date   2020/12/22
      */
-    public function __construct($indexMaxLevel)
+    public function __construct()
     {
-        //设置索引等级, 添加初始节点, 设置入口的ID
-        $this->maxLevel                   = $indexMaxLevel;
-        $header                           = new Node(-1, $indexMaxLevel);
-        $this->nodePool[$header->getID()] = $header;
-        $this->header                     = $header->getID();
+        //初始化跳表,并为初始化节点填充默认的: 数据,索引,和索引高度
+        $nextNode   = array_fill(0, $this->indexMaxLevel, null);
+        $node       = new Node2(-1, $nextNode, $this->indexMaxLevel);
+        $this->head = $node;
     }
 
 
-    public function insert($value)
+    /**
+     * 创建随机跳表层级
+     * $p = 0.5 : 概率趋向于每2个元素提取一级索引
+     * $p = 0.33 : 概率趋向于每3个元素提取一级索引
+     * $p = 0.25 : 概率趋向于每4个元素提取一级索引
+     * ...
+     * `0xFFFF` = 65536, 参考redis zslRandomLevel函数
+     * @return int
+     * @author liuhao
+     * @date   2020/12/22
+     */
+    public function randomLevel()
     {
-        $visitTrace = [];
-
-        $count = 0;
-
-        $tmp = $this->nodePool[$this->header] ?? null;
-
-        for ($i = $this->maxLevel - 1; $i >= 0; $i--) {
-            while ($tmp && $tmp->forward[$i]) {
-                $count++;
-                if ($count > 20) {
-                    break;
-                }
-                $forward = $this->nodePool[$tmp->forward[$i]];
-
-                if ($forward->value < $value) {
-                    $tmp = $forward;
-                } else {
-                    if ($forward->value > $value) {
-                        break;
-                    } else {
-                        return false;
-                    }
-                }
-            }
-            if ($tmp) {
-                $visitTrace[$i] = $tmp->getID();
-            }
+        $level = 1;
+        $p     = 0.33;
+        while ((rand() & 0xFFFF) < $p * 0xFFFF) {
+            $level += 1;
         }
 
-        $level   = $this->randomLevel();
-        $newNode = new Node($value, $level);
-        $this->addToNodePool($newNode->getID(), $newNode);
+        return ($level < $this->indexMaxLevel) ? $level : $this->indexMaxLevel;
+    }
 
+    /**
+     * 插入数据
+     * 不管怎么样传数据, 插入后都会变成有序的
+     *
+     * 跳表示例:
+     * 如下跳表, $levelCount应该等于3
+     * 1---------->9  索引层 第2级
+     * 1---->5---->9  索引层 第1级
+     * 1->3->5->7->9  数据层 第0级
+     * @param $data
+     * @return $this
+     * @author liuhao
+     * @date   2020/12/22
+     */
+    public function insert($data)
+    {
+        /**
+         * 计算本次插入的数据, 会更新到几级跳表索引, 最低为1, 因为跳表是从0开始的, 故计算时此level需要减1
+         * 数据层是第0层, level = 1 意味着只会写入到数据层, 不会创建跳表索引
+         */
+        $level = $this->randomLevel();
+        // 等待被插入的数据, 它会出现在 从0 到 $level - 1 层中
+        $newNode = new Node2($data, array_fill(0, $level, null), $level);
+
+        // 将跳表入口赋予$p, 相当于指针
+        $p = $this->head;
+        /**
+         * 等待被更新的数据
+         * 存储的是查询出新数据存放的位置的前一个元素, 将新数据的下一个节点指向下一个元素, 将前一个元素的下一个节点指向新数据
+         * 即可完成从数据层到索引层的插入
+         */
+        $update = [];
+        // 向下查找, 每一次循环都将下移一次索引层, 直到最后一次查询到数据层结束
+        for ($i = $level - 1; $i >= 0; $i--) {
+            // 向右查找, 横向查找索引层和数据层, 查找到第一个小于新数据的元素
+            while ($p->next[$i] !== null && $p->next[$i]->data < $data) {
+                $p = $p->next[$i];
+            }
+            // 将第一个小于新数据的元素保存下来
+            $update[$i] = $p;
+        }
+
+        /**
+         * 更新第一个小于新数据元素指针, 插入新元素
+         */
         for ($i = 0; $i < $level; $i++) {
-            $trace                = $this->getFromNodePool($visitTrace[$i]);
-            $newNode->forward[$i] = $trace->forward[$i];
-            $trace->forward[$i]   = $newNode->getID();
+            $newNode->next[$i]    = $update[$i]->next[$i];
+            $update[$i]->next[$i] = $newNode;
+        }
+
+        // 如果当前随机的level 大于当前跳表的level, 更新跳表的level
+        if ($level > $this->levelCount) {
+            $this->levelCount = $level;
+        }
+
+        return $this;
+    }
+
+    /**
+     *查找数据
+     * @param  int  $data
+     * @return false|mixed
+     * @author liuhao
+     * @date   2020/12/22
+     */
+    public function find(int $data)
+    {
+        $p = $this->head;
+        for ($i = $this->levelCount - 1; $i >= 0; $i--) {
+            while ($p->next[$i] !== null && $p->next[$i]->data < $data) {
+                $p = $p->next[$i];
+            }
+        }
+        if ($p->next[0] !== null && $p->next[0]->data == $data) {
+            return $p->next[0];
+        }
+
+        return false;
+    }
+
+    /**
+     *删除节点
+     * @param $data
+     * @return bool
+     * @author liuhao
+     * @date   2020/12/22
+     */
+    public function delete($data)
+    {
+        // 跳表入口
+        $p = $this->head;
+        /**
+         * 记录该元素所在层级的前一个元素, 存储的元素并不一定指向要删除的元素
+         */
+        $update = [];
+        for ($i = $this->levelCount - 1; $i >= 0; $i--) {
+            while ($p->next[$i] !== null && $p->next[$i]->data < $data) {
+                $p = $p->next[$i];
+            }
+            $update[$i] = $p;
+        }
+
+        /**
+         * 如果没有找到该元素
+         */
+        if ($p->next[0] === null || $p->next[0]->data != $data) {
+            return false;
+        }
+
+        /**
+         * 该元素最多会出现在它对应的maxLevel-1层中, 所以从这一层往下查找
+         * $update中存储的都是 第一个小于 $data的指针, 因此, 下一个元素不一定是$data, 所以需要判断一下
+         */
+        $pLevel = $p->next[0]->maxLevel;
+        for ($i = $pLevel - 1; $i >= 0; $i--) {
+            if ($update[$i]->next[$i] !== null && $update[$i]->next[$i]->data == $data) {
+                $update[$i]->next[$i] = $update[$i]->next[$i]->next[$i];
+            }
+
+        }
+        /**
+         * 当被删除的元素处于最大层级时, 才检查删除该元素后是否需要减少层级
+         */
+        if ($this->levelCount == $pLevel) {
+            // 当前层级的没有其他元素了, 说明该层级已空, 需要将当前最大层级减1
+            while ($this->levelCount > 1 && $this->head->next[$this->levelCount - 1] === null) {
+                $this->levelCount--;
+            }
         }
 
         return true;
     }
 
-    public function addToNodePool($id, $object)
+    /**
+     *打印跳表的节点和索引
+     * @return void
+     * @author liuhao
+     * @date   2020/12/22
+     */
+    public function printAll()
     {
-        $this->nodePool[$id] = $object;
-    }
+        echo PHP_EOL;
 
-    public function getFromNodePool($id)
-    {
-        return isset($this->nodePool[$id]) ? $this->nodePool[$id] : null;
-    }
+        for ($i = 0; $i < $this->levelCount; $i++) {
+            $p = $this->head->next[$i];
+            echo sprintf('第 %d 级元素为: ', $i);
+            while ($p !== null) {
+                echo $p->data, '->';
 
-    public function randomLevel()
-    {
-        $level = 1;
-
-        for ($i = 1; $i < $this->maxLevel; $i++) {
-            if (rand(0, 1)) {
-                $level++;
+                $p = $p->next[$i];
             }
-        }
 
-        return $level;
+            echo PHP_EOL;
+        }
     }
 
-    public function getNodePool()
+    /**
+     *直接返回整个跳表
+     * @return Node2
+     * @author liuhao
+     * @date   2020/12/22
+     */
+    public function getHead()
     {
-        return $this->nodePool;
+        return $this->head;
     }
 }
 
-$skipList = new SkipListOnRand2(5);
+$skipList = new SkipListOnRand2();
 $skipList->insert(0);
 $skipList->insert(1);
-//$skipList->insert(2);
-//$skipList->insert(3);
-//$skipList->insert(4);
+$skipList->insert(2);
+$skipList->insert(3);
+$skipList->insert(4);
+$skipList->printAll();
 
-echo json_encode($skipList->getNodePool()).PHP_EOL;
+$skipList->insert(5);
+$skipList->insert(6);
+$skipList->insert(7);
+$skipList->insert(8);
+$skipList->insert(9);
+$skipList->printAll();
+
+//数据量太大,可能会导致PHP内存溢出
+echo json_encode($skipList->getHead()).PHP_EOL;
--- php/SkipList/Practice/SkipListOnRand3.php
+++ php/SkipList/Practice/SkipListOnRand3.php
@@ -2,171 +2,176 @@
 
 /**
  *
- * @file   SkipList4.php
+ * @file   SkipList2.php
  * @author liuhao
- * @date   2020/12/24
+ * @date   2020/12/22
  */
 
-namespace liuhao;
+namespace SkipList\Practice;
 
 /**
- *节点
- * @class   Node
- * @author  liuhao
- * @date    2020/12/24
- * @package liuhao
+ *跳表节点
+ * @class  SNode
+ * @author liuhao
+ * @date   2020/12/22
  */
 class Node
 {
     /**
-     * @var int
+     * 数据域
+     * @var mixed|null
      */
     public $data;
-    /**
-     * @var int
-     */
-    public $maxLevel;
-    /**
-     * @var Node[]
-     */
-    public $forward;
 
     /**
-     *
-     * Node constructor.
-     * @param int $data
-     * @param array $forward
-     * @param int $maxLevel
-     * @author liuhao
-     * @date   2020/12/24
+     * 指针域，引用SNode对象
+     * @var array
      */
-    public function __construct($data, $forward = [], $maxLevel = 1)
+    public $next = [];
+
+    public function __construct($data = null)
     {
         $this->data = $data;
-        $this->forward = $forward;
-        $this->maxLevel = $maxLevel;
     }
 
+    /**
+     * 索引最大层数
+     * @return int
+     * @author liuhao
+     * @date   2020/12/22
+     */
+    public function getMaxLevel()
+    {
+        return count($this->next) - 1;
+    }
 }
 
 /**
  *跳表
- * @class   SkipList4
- * @author  liuhao
- * @date    2020/12/24
- * @package liuhao
+ * @class  SkipList
+ * @author liuhao
+ * @date   2020/12/22
  */
-class SkipListOnRand3
+class SkipList
 {
     /**
-     * 跳表入口,每一级索引层的入口和数据层的入口
+     * 索引最大层数
+     * @var int
+     */
+    public $indexLevel;
+
+    /**
+     * 头节点
      * @var Node
      */
-    public $head;
-
-    /**
-     *  当前跳表已经随机的最大层级
-     * @var int
-     */
-    public $levelCount;
-
-    /**
-     * 最大层级, 2^16=65536, 2^32=42亿+, 当16不够用时可选择更改
-     * @var int
-     */
-    public $maxLevel = 16;
+    protected $head;
 
     /**
      *
-     * SkipList4 constructor.
+     * SkipList constructor.
+     * @param  int  $indexLevel
      * @author liuhao
-     * @date   2020/12/24
+     * @date   2020/12/22
      */
-    public function __construct()
+    public function __construct(int $indexLevel)
     {
-        //初始化一个节点,同时为其填充数据,设置最大索引层的限制
-        $node = new Node(-1, array_fill(0, $this->maxLevel, null), $this->maxLevel);
-        $this->head = $node;
+        $this->indexLevel = max($indexLevel, 0);
+        $this->head       = new Node();
     }
 
     /**
-     * 创建随机跳表层级
-     * $p = 0.5 : 概率趋向于每2个元素提取一级索引
-     * $p = 0.33 : 概率趋向于每3个元素提取一级索引
-     * $p = 0.25 : 概率趋向于每4个元素提取一级索引
-     * ...
-     * `0xFFFF` = 65536, 参考redis zslRandomLevel函数
-     * @return int
-     */
-    public function randomLevel()
-    {
-
-        $level = 1;
-        $p = 0.33;
-        while ((rand() & 0xFFFF) < $p * 0xFFFF) {
-            $level += 1;
-        }
-
-        return ($level < $this->maxLevel) ? $level : $this->maxLevel;
-    }
-
-    /**
-     *插入数据,插入的数据对链表而言将会是顺序的
+     *添加数据
      * @param $data
-     * @return Node|null
+     * @return Node
      * @author liuhao
-     * @date   2020/12/24
+     * @date   2020/12/22
      */
-    public function insert($data): ?Node
+    public function addData($data)
     {
-        //获取随机的索引层, 即从0层开始一直构建到第几层
-        $level = $this->randomLevel();
-
-        //准备一个新的节点,寄存数据, 同时为新的节点填充索引层的默认数据
-        $newNode = new Node($data, array_fill(0, $level, null), $level);
-
-        //将跳表入口赋值,作为指针入口,就从这里开始逐个遍历
-        $p = $this->head;
-
-        //存储的是查询出新数据将要存放位置的前一个元素,将新数据的下一个节点指向下一个元素, 将前一个元素的下一个节点指向新数据,即可完成从数据层到索引层的插入
-        $update = [];
-
-        //从上向下查找索引层,每一次循环都将下移一次索引层,直到最后一次查询到数据层结束
-        for ($i = $level - 1; $i >= 0; $i--) {
-            //向右查找, 横向查找索引层和数据层,查找到第一个小于新数据的元素
-            while ($p->forward[$i] != null && $p->forward[$i]->data < $data) {
-                //从$i这个位置往后的节点都会被保存下来,因为这是一条链
-                $p = $p->forward[$i];
+        $newNode = new Node($data);
+        for ($level = $this->getRandomLevel(), $node = $this->head; $level >= 0; $level--) {
+            while (isset($node->next[$level]) && $data < $node->next[$level]->data) {
+                $node = $node->next[$level];
             }
-            //将第一个小于新数据的元素保存下来
-            $update[$i] = $p;
+            if (isset($node->next[$level])) {
+                $newNode->next[$level] = $node->next[$level];
+            }
+            $node->next[$level] = $newNode;
         }
-
-        //更新第一个小于新数据元素指针,插入新的节点
-        for ($i = 0; $i < $level; $i++) {
-            $newNode->forward[$i] = $update[$i]->foreard[$i];
-            $update[$i]->forward[$i] = $newNode;
-        }
-
-        // 如果当前随机的level 大于当前跳表的level, 更新跳表的level
-        if ($level > $this->levelCount) {
-            $this->levelCount = $level;
-        }
-
-
         return $newNode;
     }
 
-    public function printList()
+    /**
+     *删除数据
+     * @param $data
+     * @return bool
+     * @author liuhao
+     * @date   2020/12/22
+     */
+    public function deleteData($data)
+    {
+        $deleted = false;
+        for ($level = $this->head->getMaxLevel(), $node = $this->head; $level >= 0; $level--) {
+            while (isset($node->next[$level]) && $data < $node->next[$level]->data) {
+                $node = $node->next[$level];
+            }
+            if (isset($node->next[$level]) && $data == $node->next[$level]->data) {
+                $node->next[$level] = isset($node->next[$level]->next[$level]) ?
+                    $node->next[$level]->next[$level] : null;
+                $deleted            = true;
+            }
+        }
+        return $deleted;
+    }
+
+    /**
+     *查找数据
+     * @param $data
+     * @return false|mixed
+     * @author liuhao
+     * @date   2020/12/22
+     */
+    public function findData($data)
+    {
+        for ($level = $this->head->getMaxLevel(), $node = $this->head; $level >= 0; $level--) {
+            while (isset($node->next[$level]) && $data < $node->next[$level]->data) {
+                $node = $node->next[$level];
+            }
+            if (isset($node->next[$level]) && $data == $node->next[$level]->data) {
+                return $node->next[$level];
+            }
+        }
+        return false;
+    }
+
+    /**
+     *获取随机节点层
+     * @return int
+     * @author liuhao
+     * @date   2020/12/22
+     */
+    protected function getRandomLevel()
+    {
+        return mt_rand(0, $this->indexLevel);
+    }
+
+    /**
+     *打印跳表的节点和索引
+     * @return void
+     * @author liuhao
+     * @date   2020/12/22
+     */
+    public function printAll()
     {
         echo PHP_EOL;
-        echo PHP_EOL;
-        for ($i = 0; $i < $this->levelCount; $i++) {
-            $p = $this->head->forward[$i];
-            echo "第{$i}级元素为:";
-            while ($p != null) {
-                echo $p->data . "->";
-                $p = $p->forward[$i];
+
+        for ($i = 0; $i < $this->indexLevel; $i++) {
+            $p = $this->head->next[$i];
+            echo sprintf('第 %d 级元素为: ', $i);
+            while ($p !== null) {
+                echo $p->data, '->';
+
+                $p = $p->next[$i];
             }
 
             echo PHP_EOL;
@@ -174,11 +179,17 @@ class SkipListOnRand3
     }
 }
 
-$skipList = new SkipListOnRand3();
-$skipList->insert(0);
-$skipList->insert(1);
-$skipList->insert(2);
-$skipList->insert(3);
-$skipList->insert(4);
+/**
+ * 示例
+ */
 
-$skipList->printList();
+$indexLevel = 3;
+$skipList = new SkipList($indexLevel);
+
+for ($i = 10; $i >= 0; $i--) {
+    $skipList->addData($i);
+}
+
+//打印0到10组成的跳表
+var_dump($skipList);
+$skipList->printAll();;
